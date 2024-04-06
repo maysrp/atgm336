cb32565deeba739f08e07d7a8d3a4668c7fddeb4
Xiaosea <maysrp@gmail.com>
2024-04-06 11:22:04 +0800

    Create atgm336.py

---
+++ atgm336.py
@@ -0,0 +1,63 @@
+class gps(object):
+    def __init__(self,uart):
+        self.uart=uart
+        self.gps=[]
+    def gll(self):
+        i=self.uart.readline()
+        try:
+            if i.startswith(b"$GNGLL"):
+                line = str(i).split(',')
+                if (len(line[1][:2])!=0 and len(line[1][2:])!=0):
+                    self.Lat = float(line[1][:2])+float(line[1][2:])/60
+                    self.Tn=line[2]
+                    self.Lng = float(line[3][:3])+float(line[3][3:])/60
+                    self.Gn=line[4]
+                    self.hh=line[5][0:2]
+                    self.mm=line[5][2:4]
+                    self.ss=line[5][4:6]
+                    self.gps=[self.Lat,self.Tn,self.Lng,self.Gn,[self.hh,self.mm,self.ss]]
+                    return self.gps
+            else:
+                return False
+        except:
+            return False
+    def rmc(self):
+        i=self.uart.readline()
+        try:
+            if i.startswith(b"$GNRMC"):
+                line = str(i).split(',')
+                print(line)
+                if len(line)==14:
+                    return self.info(line)
+                elif len(line)>=7:
+                    return self.info_gps(line)
+            return False
+        except:
+            return False
+    def info(self,line):
+        self.hh=line[1][0:2]
+        self.mm=line[1][2:4]
+        self.ss=line[1][4:6]
+        self.Lat = float(line[3][:2])+float(line[3][2:])/60
+        self.Tn=line[2]
+        self.Lng = float(line[5][:3])+float(line[5][3:])/60
+        self.Gn=line[4]
+        self.gps=[self.Lat,self.Tn,self.Lng,self.Gn,(self.hh,self.mm,self.ss)]
+        self.speed=float(line[7])*1.852
+        self.date=[line[9][4:6],line[9][2:4],line[9][0:2]]
+        self.jd=line[8]
+        self.year=line[9][4:6]
+        self.month=line[9][2:4]
+        self.day=line[9][0:2]
+        return [self.Lat,self.Tn,self.Lng,self.Gn,[self.hh,self.mm,self.ss],[self.year,self.month,self.day],self.speed,self.jd]
+    def info_gps(self,line):
+        self.hh=line[1][0:2]
+        self.mm=line[1][2:4]
+        self.ss=line[1][4:6]
+        self.Lat = float(line[3][:2])+float(line[3][2:])/60
+        self.Tn=line[2]
+        self.Lng = float(line[5][:3])+float(line[5][3:])/60
+        self.Gn=line[4]
+        self.gps=[self.Lat,self.Tn,self.Lng,self.Gn,[self.hh,self.mm,self.ss]]
+        return self.gps
+
